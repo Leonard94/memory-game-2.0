ee78f93e3999aa5d687f11b8bf223734f2f68c4c
Владислав Коледа <vladislav@MacBook-Pro-Vladislav.local>
2024-11-02 17:47:30 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Leonard94/', 
+  base: '/memory-game-2.0/',
   plugins: [react()],
 })
